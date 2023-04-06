db815be7c4b89a6d465978fc74f07e901f319551
Tirth <tirthghelani0701@gmail.com>
2023-04-06 10:06:20 +0530

    create get routes

--- controller/fileuploadcontroller.js
+++ controller/fileuploadcontroller.js
@@ -1,47 +1,70 @@
-const SingleFile=require("../models/singleFile") 
-const Multifile=require("../models/multiplefile")
+const SingleFile = require("../models/singleFile")
+const Multifile = require("../models/multiplefile")
 
-const singleFileupload=async(req,res,next)=>{
+const singleFileupload = async (req, res, next) => {
     try {
         const file = new SingleFile({
-            fileName:req.file.originalname,
-            filePath:req.file.path,
-            fileType:req.file.mimetype,
-            fileSize:req.file.size
+            fileName: req.file.originalname,
+            filePath: req.file.path,
+            fileType: req.file.mimetype,
+            fileSize: req.file.size
         })
         await file.save();
-        res.status(200).send("file upload succesfully...")      
+        res.status(200).send("file upload succesfully...")
     } catch (error) {
         res.status(400).send(error.message)
     }
 }
-const multifileupload=async(req,res,next)=>{
+const multifileupload = async (req, res, next) => {
     try {
-        const fileArray=[];
+        const fileArray = [];
         req.files.forEach(element => {
-            const file={
-                fileName:element.originalname,
-                filePath:element.path,
-                fileType:element.mimetype,
-                fileSize:element.size
+            const file = {
+                fileName: element.originalname,
+                filePath: element.path,
+                fileType: element.mimetype,
+                fileSize: element.size
             }
             fileArray.push(file)
         });
-        const multifile=new Multifile({
-            title:req.body.title,
-            files:fileArray
+        const multifile = new Multifile({
+            title: req.body.title,
+            files: fileArray
         })
 
         await multifile.save();
-        
+
         res.status(200).send("multifile uploaded")
-        
+
     } catch (error) {
-        res.status(400).send(error.message) 
+        res.status(400).send(error.message)
     }
 }
 
-module.exports={
+const getsinglefile = async (req, res, next) => {
+    try {
+        const files = await SingleFile.find();
+        res.status(200).send(files)
+
+    } catch (error) {
+        res.status(400).send("can not get file")
+    }
+}
+
+const getmultifile = async (req, res, next) => {
+    try {
+        const files = await Multifile.find();
+        res.status(200).send(files)
+
+    } catch (error) {
+        res.status(400).send("can not get file")
+    }
+}
+
+
+module.exports = {
     singleFileupload,
-    multifileupload
+    multifileupload,
+    getsinglefile,
+    getmultifile
 }
--- routes/file-upload-route.js
+++ routes/file-upload-route.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const {upload}=require("../helpers/fileHelper")
-const {singleFileupload,multifileupload}=require("../controller/fileuploadcontroller")
+const {singleFileupload,multifileupload, getsinglefile, getmultifile}=require("../controller/fileuploadcontroller")
 const router=express.Router();
 
 router.post('/singleFile',upload.single('file'),singleFileupload);
 router.post("/multiFile",upload.array('files'),multifileupload);
+router.get("/getsinglefile",getsinglefile);
+router.get("/getmultifile",getmultifile)
 
 
 module.exports={
